0b002fd5d37d1aab4226a1b43ae19a15e314c534
DESKTOP-XIAOMI-CJK <DESKTOP-XIAOMI-CJK@DESKTOP-B011Q7F>
2019-06-04 08:59:26 +0800

    添加拦截器

---
+++ src/main/java/com/cjk/interceptor/UserInterceptor.java
@@ -0,0 +1,43 @@
+package com.cjk.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * SpringMVC拦截器实现
+ * @author DESKTOP-XIAOMI-CJK
+ *
+ */
+public class UserInterceptor implements HandlerInterceptor{
+
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		String userName = (String) session.getAttribute("names");
+		if(userName != null) {
+			session.setAttribute("names", userName);
+			return true;
+		}
+		request.getRequestDispatcher("login.jsp").forward(request, response);
+		//response.sendRedirect(request.getContextPath()+"/login");
+		return false;
+	}
+
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
